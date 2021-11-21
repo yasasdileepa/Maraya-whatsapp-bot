0982f0bf0c8a22bd11fd910bc530f0a193569c91
✬  RED MAKQUIL ✬ <93124334+RedMakquil@users.noreply.github.com>
2021-11-21 09:03:46 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'ASENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'MARAYA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
